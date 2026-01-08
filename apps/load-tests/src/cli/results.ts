25c937bd1278e2606b6666d6dfda3004fd882c49
Tamas <soostamas.hu@gmail.com>
2026-01-08 10:49:43 +0100

    tidy

--- apps/load-tests/src/cli/results.ts
+++ apps/load-tests/src/cli/results.ts
@@ -4,9 +4,13 @@ import { Command } from "commander";
 const program = new Command();
 
 program
-	.name("results:aggregate")
+	.name("results")
+	.description("Process and aggregate load test results")
+	.version("0.0.1");
+
+program
+	.command("aggregate")
 	.description("Aggregate results from multiple load test runs")
-	.version("0.0.1")
 	.requiredOption("--input <dir>", "Directory containing result JSON files")
 	.action((options) => {
 		console.log("[results] Aggregate results");
@@ -15,4 +19,3 @@ program
 	});
 
 program.parse();
-
