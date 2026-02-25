e5fd7d5ccabf13aaa205c58c6fca40a3749a557a
Tamas <soostamas.hu@gmail.com>
2026-02-25 09:35:21 +0100

    fix: format long import lines to pass biome CI check

--- packages/dapp-client/src/handlers/trusted-connection-handler.ts
+++ packages/dapp-client/src/handlers/trusted-connection-handler.ts
@@ -1,4 +1,12 @@
-import { ClientState, ErrorCode, type HandshakeOfferPayload, type Session, SessionError, type SessionRequest, validateSecp256k1PublicKey } from "@metamask/mobile-wallet-protocol-core";
+import {
+	ClientState,
+	ErrorCode,
+	type HandshakeOfferPayload,
+	type Session,
+	SessionError,
+	type SessionRequest,
+	validateSecp256k1PublicKey,
+} from "@metamask/mobile-wallet-protocol-core";
 import { base64ToBytes } from "@metamask/utils";
 import { HANDSHAKE_TIMEOUT } from "../client";
 import type { IConnectionHandler } from "../domain/connection-handler";
